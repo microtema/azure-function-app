b8734f913ce147f0dee874c6e89ccd382eaccc6d
TEMAMAR <mario.tema@exxeta.com>
2022-03-29 11:27:30 +0200

    MT:1000 implement key vault #comment ignore *.env

--- service-bus-topic-trigger/index.ts
+++ service-bus-topic-trigger/index.ts
@@ -1,7 +1,8 @@
-import { AzureFunction, Context } from "@azure/functions"
+import {AzureFunction, Context} from "@azure/functions"
 
 const serviceBusTopicTrigger: AzureFunction = async function(context: Context, message: any): Promise<void> {
     context.log('ServiceBus topic trigger function processed message', message);
+    return Promise.resolve()
 };
 
 export default serviceBusTopicTrigger;
